8900f071f356f22fa43a640a8359cb6269c03fef
Neanatolik <TimofeevAnVl@yandex.ru>
2024-04-01 20:43:37 +0300

    test2

--- gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ gateway/src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -5,12 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import ru.practicum.shareit.item.model.Item;
-import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
 
 @Data
 @Builder
@@ -19,9 +15,6 @@ import java.util.List;
 public class ItemRequestDto {
     private Long id;
     private String description;
-    private User requestor;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime created;
-    @Builder.Default
-    private List<Item> items = Collections.emptyList();
 }
